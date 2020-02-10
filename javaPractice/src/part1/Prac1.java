fcaada5e992a360b2b8dd1508558747291d07b92
jinju <pjj2818@gmail.com>
2020-02-10 22:38:47 +0900

    0210 commit

--- javaPractice/src/part1/Prac1.java
+++ javaPractice/src/part1/Prac1.java
@@ -3,7 +3,26 @@ package part1;
 public class Prac1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-	}
+		//System.out.println("Hello");	
+		
+		int x = 1;
+		int y = 2;
+		int z = x + y;
+		
+		System.out.println(z);	//3
+		
+		int value = 10;
+		int result = value + 10;
+		
+		System.out.println(result);	//20
+		
+		//변수는 선언된 블록 내에서만 사용이 가능함
+		
+		int v1 = 15;	//메소드 블록에서 선언
+		if(v1 > 10) {
+			int v2 = v1 - 10;	//if블록에서 선언
+		}
+			//int v3 = v1 + v2 + 5;	//v2 변수를 사용할 수 없으므로 컴파일 에러가 생김
+	}//END main
 
-}
+}//END Prac1	//~p.37
---
+++ javaPractice/src/part1/Prac2.java
@@ -0,0 +1,39 @@
+package part1;
+
+public class Prac2 {
+
+	public static void main(String[] args) {
+		byte var1 = 125;
+		int var2 = 125;
+		
+		for(int i = 0; i < 5; i++) {
+			var1++;	//127이 넘어서는 순간 최소값인 -128부터 다시 저장됨
+			var2++;
+			System.out.println("var1 : " + var1 + "\t" + "var2 : " + var2);
+		}//END for
+		
+		boolean stop = true;
+		
+		if(stop) {
+			System.out.println("중지합니다.");
+		} else {
+			System.out.println("시작합니다.");
+		}//END if
+		
+		byte byteValue = 10;
+		int intValue = byteValue;
+		System.out.println(intValue);
+		
+		int i = 128;
+		
+		if(i < Byte.MIN_VALUE || i > Byte.MAX_VALUE) {
+			System.out.println("byte 타입으로 변환할 수 없습니다.");
+			System.out.println("값을 다시 확인해 주세요.");
+		} else {
+			byte b = (byte) i;
+			System.out.println(b);
+		}//END if
+		
+	}//END main
+
+}//END Prac2	//~p.63
