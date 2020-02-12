eb10d634d96dd034cbd5859a9870eb35993d34aa
jinju <pjj2818@gmail.com>
2020-02-12 10:07:30 +0900

    0212 commit

--- javaPractice/src/part3/Prac1.java
+++ javaPractice/src/part3/Prac1.java
@@ -3,6 +3,7 @@ package part3;
 public class Prac1 {
 
 	public static void main(String[] args) {
+		//Student클래스의 인스턴스들. 완전히 독립된 서로 다른 객체
 		Student s1 = new Student();
 		Student s2 = new Student();
 	}
