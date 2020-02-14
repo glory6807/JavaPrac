8c5b63ad80804749cde50670f415aee4092cbecf
jinju <pjj2818@gmail.com>
2020-02-15 00:07:33 +0900

    0215 commit

---
+++ javaPractice/src/part5/Earth.java
@@ -0,0 +1,11 @@
+package part5;
+
+public class Earth {
+	//static final 필드는 객체마다 저장되지 않고, 클래스에만 포함되고, 한 번 초기값이 저장되면 변경할 수 없음
+	static final double Earth_radius = 6400;
+	static final double Earth_surface_area;
+	
+	static {
+		Earth_surface_area = 4 * Math.PI * Earth_radius * Earth_radius;
+	}
+}
---
+++ javaPractice/src/part5/PracS.java
@@ -0,0 +1,13 @@
+package part5;
+
+public class PracS {
+
+	public static void main(String[] args) {
+		//Singleton obj1 = new Singleton();	//컴파일에러
+		//Singleton obj2 = new Singleton();	//컴파일에러
+		
+		
+		
+	}
+
+}
