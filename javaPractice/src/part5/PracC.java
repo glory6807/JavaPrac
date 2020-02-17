c0c540ce1f1b2dba46115feef9ebc8f4bc230a72
jinju <pjj2818@gmail.com>
2020-02-17 22:21:23 +0900

    0217 commit

---
+++ javaPractice/src/part5/PracC.java
@@ -0,0 +1,18 @@
+package part5;
+
+public class PracC {
+
+	public static void main(String[] args) {
+		Car car = new Car();
+		car.setSpeed(50);
+		
+		System.out.println("현재 속도 : " + car.getSpeed());
+		
+		if(!car.isStop()) {
+			car.setStop(true);
+		}
+		
+		System.out.println("현재 속도 : " + car.getSpeed());
+	}
+
+}
