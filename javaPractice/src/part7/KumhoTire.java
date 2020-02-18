dcabddb5726d8918671f14a5149b31d5a0d11490
jinju <pjj2818@gmail.com>
2020-02-18 11:38:33 +0900

    0218 commit

---
+++ javaPractice/src/part7/KumhoTire.java
@@ -0,0 +1,24 @@
+package part7;
+
+public class KumhoTire extends Tire{
+	//필드
+	
+	//생성자
+	public KumhoTire(int maxRotation, String location) {
+		super(maxRotation, location);
+	}
+
+	@Override
+	public boolean roll() {
+		++accumulateRotation;
+		if(accumulateRotation<maxRotation) {
+			System.out.println(location + "KumhoTire 수명 " + (maxRotation-accumulateRotation) + "회");
+			return true;
+		} else {
+			System.out.println("*** " + location + " KumhoTire 펑크 ***");
+			return false;
+		}
+	}
+	
+	
+}
