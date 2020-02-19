96bb362afb3f944cd17a5f9e22aff18432cd5228
jinju <pjj2818@gmail.com>
2020-02-19 14:30:35 +0900

    0219 commit

---
+++ javaPractice/src/part8/PracP.java
@@ -0,0 +1,15 @@
+package part8;
+
+public class PracP {
+
+	public static void main(String[] args) {
+		//Phone phone = new Phone();
+		
+		SmartPhone smartPhone = new SmartPhone("홍길동");
+		
+		smartPhone.turnOn();
+		smartPhone.internetSearch();
+		smartPhone.turnOff();
+	}
+
+}
