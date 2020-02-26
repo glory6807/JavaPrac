323d6584abd799395675c7f0ac1b3fc6ea1974e4
jinju <pjj2818@gmail.com>
2020-02-26 10:42:56 +0900

    0226 commit

---
+++ javaPractice/src/part10/PracA.java
@@ -0,0 +1,28 @@
+package part10;
+
+public class PracA {
+
+	public static void main(String[] args) {
+		Anonymous anony = new Anonymous();
+		//1. 익명 객체 필드 사용
+		anony.field.wake();
+		
+		//2. 익명 객체 로컬 변수 사용
+		anony.method1();
+		
+		//3. 익명 객체 매개값 사용
+		anony.method2(
+			new Person() {
+				void study() {
+					System.out.println("공부합니다.");
+				}
+				@Override
+				void wake() {
+					System.out.println("8시에 일어납니다.");
+					study();
+				}
+			}
+		);	//END method2()
+	}
+
+}
